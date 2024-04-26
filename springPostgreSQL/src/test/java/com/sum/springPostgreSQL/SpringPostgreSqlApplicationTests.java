e08ceddb37ceed874b09d4f2a4d2fbfacfc4cfd5
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-04-27 01:02:26 +0530

    springPostgreSQL starter project added

---
+++ springPostgreSQL/src/test/java/com/sum/springPostgreSQL/SpringPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum.springPostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
