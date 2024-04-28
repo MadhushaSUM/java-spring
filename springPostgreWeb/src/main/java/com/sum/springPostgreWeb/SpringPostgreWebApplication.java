21daa0d9a1d1a10b4700ac27dd1596c938bfeebd
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-04-28 21:01:08 +0530

    Initial SpringPostgreWeb project added

---
+++ springPostgreWeb/src/main/java/com/sum/springPostgreWeb/SpringPostgreWebApplication.java
@@ -0,0 +1,13 @@
+package com.sum.springPostgreWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgreWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgreWebApplication.class, args);
+	}
+
+}
---
+++ springPostgreWeb/src/test/java/com/sum/springPostgreWeb/SpringPostgreWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum.springPostgreWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgreWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
