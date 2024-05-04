bbe9b432af4fc5b61321e5f1c40ac5153cfdde81
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-05-05 00:24:20 +0530

    Update AppSecurityConfig.java

--- springSecurityApp/src/main/java/com/sum/springSecurityApp/AppSecurityConfig.java
+++ springSecurityApp/src/main/java/com/sum/springSecurityApp/AppSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -30,7 +31,7 @@ public class AppSecurityConfig {
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
 
         return provider;
     }
