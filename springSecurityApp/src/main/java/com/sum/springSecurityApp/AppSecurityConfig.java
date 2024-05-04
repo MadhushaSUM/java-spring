5e850315567730d23d84754d4a57f27df33b3182
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-05-04 22:57:15 +0530

    Fetch users from postgre database

--- springSecurityApp/src/main/java/com/sum/springSecurityApp/AppSecurityConfig.java
+++ springSecurityApp/src/main/java/com/sum/springSecurityApp/AppSecurityConfig.java
@@ -1,20 +1,37 @@
 package com.sum.springSecurityApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppSecurityConfig {
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("abc")
+//                .password("123")
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("abc")
-                .password("123")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public AuthenticationProvider authProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return provider;
     }
 }
---
+++ springSecurityApp/src/main/java/com/sum/springSecurityApp/UserRepo.java
@@ -0,0 +1,9 @@
+package com.sum.springSecurityApp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, String> {
+    User findByUsername(String username);
+}
