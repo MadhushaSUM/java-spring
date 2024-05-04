c65a987d98044d323a1a9bc255abd3884372e91f
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-05-05 00:32:03 +0530

    Initial code of monolithic quiz app

---
+++ quizAppMonolothic/src/main/java/com/sum/quizAppMonolothic/QuizAppMonolothicApplication.java
@@ -0,0 +1,13 @@
+package com.sum.quizAppMonolothic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppMonolothicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppMonolothicApplication.class, args);
+	}
+
+}
---
+++ quizAppMonolothic/src/test/java/com/sum/quizAppMonolothic/QuizAppMonolothicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum.quizAppMonolothic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppMonolothicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
