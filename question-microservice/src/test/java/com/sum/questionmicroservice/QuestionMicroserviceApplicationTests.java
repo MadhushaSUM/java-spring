c64aa4bddf5f5a2b5dd06aed978c178b2ef84213
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-05-23 01:35:40 +0530

    Add question microservice starting code

---
+++ question-microservice/src/test/java/com/sum/questionmicroservice/QuestionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum.questionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
